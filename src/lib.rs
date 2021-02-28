1413eacb3bc43a7050aded0a4bffefb4e8fcb7d2
John-Nagle <nagle@sitetruth.com>
2021-02-28 12:42:39 -0800

    Initial type

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//  
+//  Library for serializing and de-serializing data in
+//  Linden Lab Structured Data format.
+//
+//  Animats
+//  February, 2021.
+//  License: LGPL.
+//
+use std::collections::HashMap;
+//
+//  The main primitive data item
+//
+#[derive(Debug, Clone, PartialEq)]
+pub enum JsonValue {
+    Null,
+    Boolean(bool),
+    Real(f64),
+    UUID([u8;16]),
+    String(String),
+    Date(i64),
+    URI(String),
+    Binary(Vec<u8>),
+    Map(HashMap<String,JsonValue>),
+    Array(Vec<JsonValue>),
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
